2e6d4189c66776e3d3742e0f9475619b3d92b9ea
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-01-23 16:59:11 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {engine} = require('express-handlebars')
+
+app.engine('.hbs',engine({extname:'.hbs'}))
+app.set('view engine','.hbs')
+app.set('views','./views')
 
 app.get('/', (req, res)=>{
- res.send('Build Server Sucessfully')
+ res.render('index')
 })
+app.get('/short-url', (req, res)=>{
+ res.send('Here is result page.')
+})
+app.get('/new-url',(req, res)=>{
+  res.send('This rout should redirect to originally web')
+})
+
 
 app.listen(port,()=>{
   console.log(`express server is running on http://localhost:${port}`)
