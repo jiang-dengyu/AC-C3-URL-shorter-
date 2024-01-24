459fe3dd61dba8d3869f94abc45a822647fb8dd6
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-01-24 15:38:33 +0800

    完成製造隨機短網址，但無法連結

--- app.js
+++ app.js
@@ -3,18 +3,34 @@ const app = express()
 const port = 3000
 const {engine} = require('express-handlebars')
 
+
+function makeString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+let urls = makeString(5)
+
 app.engine('.hbs',engine({extname:'.hbs'}))
 app.set('view engine','.hbs')
 app.set('views','./views')
 
 app.get('/', (req, res)=>{
- res.render('index')
+  res.render('index')
 })
+
 app.get('/short-url', (req, res)=>{
- res.send('Here is result page.')
+  res.render('newurl', {urls})
 })
+
 app.get('/new-url',(req, res)=>{
-  res.send('This rout should redirect to originally web')
+  res.redirect(`${urls}`)
 })
 
 
